c7e66f3ba40fe228d47c4653bff22b0b235f5103
tarek fouda <tarek.fouda@barqorders.com>
2022-05-01 14:07:51 +0200

    backup

--- lib/core/routes/router_imports.gr.dart
+++ lib/core/routes/router_imports.gr.dart
@@ -10,8 +10,8 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i16;
-import 'package:flutter/material.dart' as _i17;
+import 'package:auto_route/auto_route.dart' as _i17;
+import 'package:flutter/material.dart' as _i18;
 import 'package:flutter_tdd/features/auth/presentation/pages/active_account/active_account_imports.dart'
     as _i8;
 import 'package:flutter_tdd/features/auth/presentation/pages/forget_password/forget_password_imports.dart'
@@ -36,105 +36,112 @@ import 'package:flutter_tdd/features/auth/presentation/pages/splash/splash_impor
     as _i1;
 import 'package:flutter_tdd/features/auth/presentation/pages/verify_code/verify_code_imports.dart'
     as _i9;
-import 'package:flutter_tdd/features/base/presentation/pages/profile/profile_imports.dart'
+import 'package:flutter_tdd/features/base/presentation/pages/home/home_imports.dart'
     as _i15;
+import 'package:flutter_tdd/features/base/presentation/pages/profile/profile_imports.dart'
+    as _i16;
 import 'package:flutter_tdd/features/general/presentation/pages/repeated_questions/repeated_questions_imports.dart'
     as _i14;
 import 'package:flutter_tdd/features/general/presentation/pages/terms/terms_imports.dart'
     as _i13;
 
-class AppRouter extends _i16.RootStackRouter {
-  AppRouter([_i17.GlobalKey<_i17.NavigatorState>? navigatorKey])
+class AppRouter extends _i17.RootStackRouter {
+  AppRouter([_i18.GlobalKey<_i18.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i16.PageFactory> pagesMap = {
+  final Map<String, _i17.PageFactory> pagesMap = {
     SplashRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i1.Splash());
     },
     SelectLanguageRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i2.SelectLanguage());
     },
     LoginRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i3.Login());
     },
     IntroScreensRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i4.IntroScreens());
     },
     LoginTypeRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i5.LoginType());
     },
     RestoreBackupRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i6.RestoreBackup());
     },
     RegisterRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i7.Register());
     },
     ActiveAccountRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i8.ActiveAccount());
     },
     VerifyCodeRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i9.VerifyCode());
     },
     ResetPasswordRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i10.ResetPassword());
     },
     ForgetPasswordRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i11.ForgetPassword());
     },
     GeometricScreenRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i12.GeometricScreen());
     },
     TermsRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i13.Terms());
     },
     RepeatedQuestionsRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
+      return _i17.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i14.RepeatedQuestions());
     },
+    HomeRoute.name: (routeData) {
+      return _i17.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i15.Home());
+    },
     ProfileRoute.name: (routeData) {
-      return _i16.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i15.Profile());
+      return _i17.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i16.Profile());
     }
   };
 
   @override
-  List<_i16.RouteConfig> get routes => [
-        _i16.RouteConfig(SplashRoute.name, path: '/'),
-        _i16.RouteConfig(SelectLanguageRoute.name, path: '/select-language'),
-        _i16.RouteConfig(LoginRoute.name, path: '/Login'),
-        _i16.RouteConfig(IntroScreensRoute.name, path: '/intro-screens'),
-        _i16.RouteConfig(LoginTypeRoute.name, path: '/login-type'),
-        _i16.RouteConfig(RestoreBackupRoute.name, path: '/restore-backup'),
-        _i16.RouteConfig(RegisterRoute.name, path: '/Register'),
-        _i16.RouteConfig(ActiveAccountRoute.name, path: '/active-account'),
-        _i16.RouteConfig(VerifyCodeRoute.name, path: '/verify-code'),
-        _i16.RouteConfig(ResetPasswordRoute.name, path: '/reset-password'),
-        _i16.RouteConfig(ForgetPasswordRoute.name, path: '/forget-password'),
-        _i16.RouteConfig(GeometricScreenRoute.name, path: '/geometric-screen'),
-        _i16.RouteConfig(TermsRoute.name, path: '/Terms'),
-        _i16.RouteConfig(RepeatedQuestionsRoute.name,
+  List<_i17.RouteConfig> get routes => [
+        _i17.RouteConfig(SplashRoute.name, path: '/'),
+        _i17.RouteConfig(SelectLanguageRoute.name, path: '/select-language'),
+        _i17.RouteConfig(LoginRoute.name, path: '/Login'),
+        _i17.RouteConfig(IntroScreensRoute.name, path: '/intro-screens'),
+        _i17.RouteConfig(LoginTypeRoute.name, path: '/login-type'),
+        _i17.RouteConfig(RestoreBackupRoute.name, path: '/restore-backup'),
+        _i17.RouteConfig(RegisterRoute.name, path: '/Register'),
+        _i17.RouteConfig(ActiveAccountRoute.name, path: '/active-account'),
+        _i17.RouteConfig(VerifyCodeRoute.name, path: '/verify-code'),
+        _i17.RouteConfig(ResetPasswordRoute.name, path: '/reset-password'),
+        _i17.RouteConfig(ForgetPasswordRoute.name, path: '/forget-password'),
+        _i17.RouteConfig(GeometricScreenRoute.name, path: '/geometric-screen'),
+        _i17.RouteConfig(TermsRoute.name, path: '/Terms'),
+        _i17.RouteConfig(RepeatedQuestionsRoute.name,
             path: '/repeated-questions'),
-        _i16.RouteConfig(ProfileRoute.name, path: '/Profile')
+        _i17.RouteConfig(HomeRoute.name, path: '/Home'),
+        _i17.RouteConfig(ProfileRoute.name, path: '/Profile')
       ];
 }
 
 /// generated route for
 /// [_i1.Splash]
-class SplashRoute extends _i16.PageRouteInfo<void> {
+class SplashRoute extends _i17.PageRouteInfo<void> {
   const SplashRoute() : super(SplashRoute.name, path: '/');
 
   static const String name = 'SplashRoute';
@@ -142,7 +149,7 @@ class SplashRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.SelectLanguage]
-class SelectLanguageRoute extends _i16.PageRouteInfo<void> {
+class SelectLanguageRoute extends _i17.PageRouteInfo<void> {
   const SelectLanguageRoute()
       : super(SelectLanguageRoute.name, path: '/select-language');
 
@@ -151,7 +158,7 @@ class SelectLanguageRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.Login]
-class LoginRoute extends _i16.PageRouteInfo<void> {
+class LoginRoute extends _i17.PageRouteInfo<void> {
   const LoginRoute() : super(LoginRoute.name, path: '/Login');
 
   static const String name = 'LoginRoute';
@@ -159,7 +166,7 @@ class LoginRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i4.IntroScreens]
-class IntroScreensRoute extends _i16.PageRouteInfo<void> {
+class IntroScreensRoute extends _i17.PageRouteInfo<void> {
   const IntroScreensRoute()
       : super(IntroScreensRoute.name, path: '/intro-screens');
 
@@ -168,7 +175,7 @@ class IntroScreensRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i5.LoginType]
-class LoginTypeRoute extends _i16.PageRouteInfo<void> {
+class LoginTypeRoute extends _i17.PageRouteInfo<void> {
   const LoginTypeRoute() : super(LoginTypeRoute.name, path: '/login-type');
 
   static const String name = 'LoginTypeRoute';
@@ -176,7 +183,7 @@ class LoginTypeRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i6.RestoreBackup]
-class RestoreBackupRoute extends _i16.PageRouteInfo<void> {
+class RestoreBackupRoute extends _i17.PageRouteInfo<void> {
   const RestoreBackupRoute()
       : super(RestoreBackupRoute.name, path: '/restore-backup');
 
@@ -185,7 +192,7 @@ class RestoreBackupRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i7.Register]
-class RegisterRoute extends _i16.PageRouteInfo<void> {
+class RegisterRoute extends _i17.PageRouteInfo<void> {
   const RegisterRoute() : super(RegisterRoute.name, path: '/Register');
 
   static const String name = 'RegisterRoute';
@@ -193,7 +200,7 @@ class RegisterRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i8.ActiveAccount]
-class ActiveAccountRoute extends _i16.PageRouteInfo<void> {
+class ActiveAccountRoute extends _i17.PageRouteInfo<void> {
   const ActiveAccountRoute()
       : super(ActiveAccountRoute.name, path: '/active-account');
 
@@ -202,7 +209,7 @@ class ActiveAccountRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i9.VerifyCode]
-class VerifyCodeRoute extends _i16.PageRouteInfo<void> {
+class VerifyCodeRoute extends _i17.PageRouteInfo<void> {
   const VerifyCodeRoute() : super(VerifyCodeRoute.name, path: '/verify-code');
 
   static const String name = 'VerifyCodeRoute';
@@ -210,7 +217,7 @@ class VerifyCodeRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i10.ResetPassword]
-class ResetPasswordRoute extends _i16.PageRouteInfo<void> {
+class ResetPasswordRoute extends _i17.PageRouteInfo<void> {
   const ResetPasswordRoute()
       : super(ResetPasswordRoute.name, path: '/reset-password');
 
@@ -219,7 +226,7 @@ class ResetPasswordRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i11.ForgetPassword]
-class ForgetPasswordRoute extends _i16.PageRouteInfo<void> {
+class ForgetPasswordRoute extends _i17.PageRouteInfo<void> {
   const ForgetPasswordRoute()
       : super(ForgetPasswordRoute.name, path: '/forget-password');
 
@@ -228,7 +235,7 @@ class ForgetPasswordRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i12.GeometricScreen]
-class GeometricScreenRoute extends _i16.PageRouteInfo<void> {
+class GeometricScreenRoute extends _i17.PageRouteInfo<void> {
   const GeometricScreenRoute()
       : super(GeometricScreenRoute.name, path: '/geometric-screen');
 
@@ -237,7 +244,7 @@ class GeometricScreenRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i13.Terms]
-class TermsRoute extends _i16.PageRouteInfo<void> {
+class TermsRoute extends _i17.PageRouteInfo<void> {
   const TermsRoute() : super(TermsRoute.name, path: '/Terms');
 
   static const String name = 'TermsRoute';
@@ -245,7 +252,7 @@ class TermsRoute extends _i16.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i14.RepeatedQuestions]
-class RepeatedQuestionsRoute extends _i16.PageRouteInfo<void> {
+class RepeatedQuestionsRoute extends _i17.PageRouteInfo<void> {
   const RepeatedQuestionsRoute()
       : super(RepeatedQuestionsRoute.name, path: '/repeated-questions');
 
@@ -253,8 +260,16 @@ class RepeatedQuestionsRoute extends _i16.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i15.Profile]
-class ProfileRoute extends _i16.PageRouteInfo<void> {
+/// [_i15.Home]
+class HomeRoute extends _i17.PageRouteInfo<void> {
+  const HomeRoute() : super(HomeRoute.name, path: '/Home');
+
+  static const String name = 'HomeRoute';
+}
+
+/// generated route for
+/// [_i16.Profile]
+class ProfileRoute extends _i17.PageRouteInfo<void> {
   const ProfileRoute() : super(ProfileRoute.name, path: '/Profile');
 
   static const String name = 'ProfileRoute';
--- lib/features/base/presentation/manager/routes/routes.dart
+++ lib/features/base/presentation/manager/routes/routes.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter_tdd/features/base/presentation/pages/home/home_imports.dart';
 
 import '../../pages/profile/profile_imports.dart';
 
 const List<AutoRoute> baseRoute = [
+  AutoRoute(page: Home),
   AutoRoute(page: Profile),
 ];
